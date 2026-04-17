514f1eb0cb43d0a0e93df9494319c2162edbb5e2
Hinko Kocevar <hinkocevar@gmail.com>
2026-04-17 19:38:27 +0200

    fix exe startup

--- nfc_uid/__main__.py
+++ nfc_uid/__main__.py
@@ -1,4 +1,11 @@
-from .nfc_uid import main
+if __package__:
+    from .nfc_uid import main
+else:
+    import os
+    import sys
+
+    sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+    from nfc_uid.nfc_uid import main
 
 
 if __name__ == "__main__":
